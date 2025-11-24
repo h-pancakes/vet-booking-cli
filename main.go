aacdc9a612ac101286c2d057bc9cc91df13b009e
Heshaam <heshaamzain06@outlook.com>
2025-11-24 14:17:55 +0000

    Fixed functions

--- main.go
+++ main.go
@@ -13,19 +13,18 @@ type Dog struct {
 	vaccinated bool
 }
 
-func (d *Dog) appointmentDecision() string {
-	if d.vaccinated {
-		return fmt.Sprintf(
-			"%s (%s) is vaccinated! Booking regular checkup appointment...", d.name, d.breed,
-		)
-	}
-	return fmt.Sprintf(
-		"%s (%s) is NOT vaccinated! Please book a vaccination appointment first", d.name, d.breed,
-	)
+func dogCounter(scanner *bufio.Scanner) int {
+	var dogCount int
+
+	fmt.Println("Hi and welcome to our booking service!")
+	fmt.Println("Please enter how many dogs you are booking in today: ")
+	scanner.Scan()
+	dogCount, _ = strconv.Atoi(scanner.Text())
+
+	return dogCount
 }
 
-func bookAppointment(scanner *bufio.Scanner) []Dog {
-	dogCount := dogCounter()
+func bookAppointment(scanner *bufio.Scanner, dogCount int) []Dog {
 	dogs := make([]Dog, dogCount)
 
 	for i := 0; i < dogCount; i++ {
@@ -55,19 +54,24 @@ func bookAppointment(scanner *bufio.Scanner) []Dog {
 	return dogs
 }
 
-func dogCounter(scanner *bufio.Scanner) int {
-	var dogCount int
-
-	fmt.Println("Hi and welcome to our booking service!")
-	fmt.Println("Please enter how many dogs you are booking in today: ")
-	scanner.Scan()
-	dogCount, _ = strconv.Atoi(scanner.Text())
-
-	return dogCount
+func (d *Dog) appointmentDecision() string {
+	if d.vaccinated {
+		return fmt.Sprintf(
+			"%s (%s) is vaccinated! Booking regular checkup appointment...", d.name, d.breed,
+		)
+	}
+	return fmt.Sprintf(
+		"%s (%s) is NOT vaccinated! Please book a vaccination appointment first", d.name, d.breed,
+	)
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	dogcount := dogCounter(scanner)
-	dogs := bookAppointment(scanner, dogcount)
+
+	dogCount := dogCounter(scanner)
+	dogs := bookAppointment(scanner, dogCount)
+
+	for _, d := range dogs {
+		fmt.Println(d.appointmentDecision())
+	}
 }
