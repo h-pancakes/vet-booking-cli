c988ed9509d59c3e29266cc16498a39b1e568b38
Heshaam <heshaamzain06@outlook.com>
2026-08-16 12:30:55 +0100

    Refactor data models into models.go

--- main.go
+++ main.go
@@ -13,34 +13,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// user is a struct that holds information about the user of the booking service.
-// It contains contact details persisted to the database.
-type user struct {
-	firstName string
-	lastName  string
-	phone     string
-	email     string
-}
-
-// pet is a struct that holds information about a pet that the user is booking an appointment for.
-// This information is stored in the appointments table in the database.
-type pet struct {
-	name       string
-	species    string
-	age        int
-	weightKg   float64
-	vaccinated bool
-}
-
-// appointment is a struct that holds all information related to an appointment booked by the user.
-// This information is stored in the appointments table in the database.
-type appointment struct {
-	appointmentType string
-	pet             pet
-	vet             string
-	dateTime        time.Time
-}
-
 // allowedSpecies is a list that holds the options for choosing the pet's species for the appointment.
 var allowedSpecies = []string{
 	"Dog",
